6c670269a6efa26996201eac2521f41a9e2f5125
Devon Taylor <devontaylor00@gmail.com>
2017-07-08 16:12:30 -0600

    Create Ticket and TicketType models

---
+++ phbgtu-ticketing-prototypes/Models/Ticket.cs
@@ -0,0 +1,11 @@
+﻿
+namespace phbgtu_ticketing_prototypes.Models
+{
+    public class Ticket
+    {
+		public int TicketID { get; set; }
+		public int TicketTypeID { get; set; }
+		public int EventTicketNumber { get; set; }
+		public string EventTicketCode { get; set; }
+    }
+}
---
+++ phbgtu-ticketing-prototypes/Models/TicketType.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace phbgtu_ticketing_prototypes.Models
+{
+	public class TicketType
+	{
+		public int TicketTypeID { get; set; }
+		public int EventID { get; set; }
+		public string TicketTypeName { get; set; }
+		public int TicketTypeLimit { get; set; }
+		//TODO how to set up a money field?
+
+		public ICollection<Ticket> Tickets { get; set; }
+    }
+}
