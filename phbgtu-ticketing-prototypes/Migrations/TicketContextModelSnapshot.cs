f9d1684f646e7c024c97f292b3512ab9f821c406
Devon Taylor <devontaylor00@gmail.com>
2017-07-11 09:32:38 -0600

    Another migration file

---
+++ phbgtu-ticketing-prototypes/Migrations/TicketContextModelSnapshot.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using phbgtu_ticketing_prototypes.Data;
+
+namespace phbgtu_ticketing_prototypes.Migrations
+{
+    [DbContext(typeof(TicketContext))]
+    partial class TicketContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("phbgtu_ticketing_prototypes.Models.Ticket", b =>
+                {
+                    b.Property<int>("TicketID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("EventTicketCode");
+
+                    b.Property<int>("EventTicketNumber");
+
+                    b.Property<int>("TicketTypeID");
+
+                    b.HasKey("TicketID");
+
+                    b.HasIndex("TicketTypeID");
+
+                    b.ToTable("Tickets");
+                });
+
+            modelBuilder.Entity("phbgtu_ticketing_prototypes.Models.TicketType", b =>
+                {
+                    b.Property<int>("TicketTypeID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("EventID");
+
+                    b.Property<int>("TicketTypeLimit");
+
+                    b.Property<string>("TicketTypeName");
+
+                    b.HasKey("TicketTypeID");
+
+                    b.ToTable("TicketTypes");
+                });
+
+            modelBuilder.Entity("phbgtu_ticketing_prototypes.Models.Ticket", b =>
+                {
+                    b.HasOne("phbgtu_ticketing_prototypes.Models.TicketType")
+                        .WithMany("Tickets")
+                        .HasForeignKey("TicketTypeID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
