b319a056335feed75e438e22ca83e39b87d67462
Devon Taylor <devontaylor00@gmail.com>
2017-07-11 09:30:21 -0600

    Fix database setup

--- phbgtu-ticketing-prototypes/Startup.cs
+++ phbgtu-ticketing-prototypes/Startup.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using phbgtu_ticketing_prototypes.Data;
 
 namespace phbgtu_ticketing_prototypes
 {
@@ -29,6 +27,8 @@ namespace phbgtu_ticketing_prototypes
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddDbContext<TicketContext>(options => options.UseSqlServer(Configuration.GetConnectionString("TicketContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
