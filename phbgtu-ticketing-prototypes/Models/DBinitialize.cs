a7e6dae8a2b02557366716c75df1b3f41b0d1172
Unknown <stephencoffman@mail.weber.edu>
2017-07-12 10:54:26 -0600

    Added file to inicialize DB for TicketEvent

---
+++ phbgtu-ticketing-prototypes/Models/DBinitialize.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace phbgtu_ticketing_prototypes.Models
+{
+	public static class DBinitialize
+	{
+		public static void EnsureCreated(IServiceProvider serviceProvider)
+		{
+			var context = new phbgtu_ticketing_prototypes_Context(
+				serviceProvider.GetRequiredService<DbContextOptions<phbgtu_ticketing_prototypes_Context>>());
+			context.Database.EnsureCreated();
+		}
+	}
+}
