3f41ff76b6ecb87905c0b08748206585302d774d
Unknown <stephencoffman@mail.weber.edu>
2017-07-12 09:54:43 -0600

    Added context file for working with TicketEvent Database

---
+++ phbgtu-ticketing-prototypes/Models/phbgtu_ticketing_prototypes_Context.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace phbgtu_ticketing_prototypes.Models
+{
+    public class phbgtu_ticketing_prototypes_Context
+    {
+        public phbgtu_ticketing_prototypes_Context()
+        {
+        }
+    }
+}
