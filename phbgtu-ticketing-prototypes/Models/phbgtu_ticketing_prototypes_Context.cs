7dc61370096c40896d2f286670ad3cb41e87ed75
Unknown <stephencoffman@mail.weber.edu>
2017-07-12 10:04:35 -0600

    Added to context code for TicketEvent Database use

--- phbgtu-ticketing-prototypes/Models/phbgtu_ticketing_prototypes_Context.cs
+++ phbgtu-ticketing-prototypes/Models/phbgtu_ticketing_prototypes_Context.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+
 namespace phbgtu_ticketing_prototypes.Models
 {
-    public class phbgtu_ticketing_prototypes_Context
+    public class phbgtu_ticketing_prototypes_Context : DbContext
     {
-        public phbgtu_ticketing_prototypes_Context()
+        public phbgtu_ticketing_prototypes_Context(DbContextOptions<phbgtu_ticketing_prototypes_Context> options)
+            : base (options)
         {
         }
+
+        public DbSet<phbgtu_ticketing_prototypes.Models.TicketEvent> TicketEvent { get; set; }
+
     }
 }
