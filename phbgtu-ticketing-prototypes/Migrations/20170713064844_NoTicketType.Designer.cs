ed9c49f5e8974a1769f020472ec911d14246dc0c
Devon Taylor <devontaylor00@gmail.com>
2017-07-13 00:51:37 -0600

    [Migration] Remove foreign key between ticket and ticket type
    See commit 46a9a4560aa0ca4a9d352862a0c59454a46b84e9

--- phbgtu-ticketing-prototypes/Migrations/20170713064844_NoTicketType.Designer.cs
+++ phbgtu-ticketing-prototypes/Migrations/20170713064844_NoTicketType.Designer.cs
@@ -8,9 +8,10 @@ using phbgtu_ticketing_prototypes.Data;
 namespace phbgtu_ticketing_prototypes.Migrations
 {
     [DbContext(typeof(TicketContext))]
-    partial class TicketContextModelSnapshot : ModelSnapshot
+    [Migration("20170713064844_NoTicketType")]
+    partial class NoTicketType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
