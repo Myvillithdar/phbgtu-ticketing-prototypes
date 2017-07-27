ab0c89ab59311d2c41c29cced8a72687dfe5f88e
Stephen <stephencoffman@mail.weber.edu>
2017-07-27 02:50:16 -0600

    Added ViewModel

--- phbgtu-ticketing-prototypes/Models/UserAccount.cs
+++ phbgtu-ticketing-prototypes/Models/UserAccount.cs
@@ -18,7 +18,7 @@ namespace phbgtu_ticketing_prototypes.Models
         [NotMapped]
         public UserType UserType { get; set; }
 
-        public ICollection<Ticket> Tickets { get; set; } //this is the physical one UserAccount to many Tickets
+        public ICollection<Ticket> Tickets { get; set; } //this is the physical relation - one UserAccount to many Tickets
 
         public UserAccount()
         {
